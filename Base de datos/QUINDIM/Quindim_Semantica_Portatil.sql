1eeaab4922aaa905d539b297fac3adb98b971278
Pablo Lavín <thepabloface@hotmail.com>
2019-09-11 10:19:05 -0500

    Pasense los scrip

--- Base de datos/QUINDIM/Quindim_Semantica_Portatil.sql
+++ Base de datos/QUINDIM/Quindim_Semantica_Portatil.sql
@@ -1,3 +1,5 @@
 USE PROTOLENGUAJE
 DELETE FROM ReglasSemanticas
 INSERT INTO ReglasSemanticas VALUES('TDD1 INTE','INTD')
+INSERT INTO ReglasSemanticas VALUES('TDD5 BOOL','BOLD')
+SELECT * FROM ReglasSemanticas
