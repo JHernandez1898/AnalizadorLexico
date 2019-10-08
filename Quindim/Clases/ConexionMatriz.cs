2faf070c01e00e9fa4e03c35d9146f1869d472bf
Julio Hernández <armando10gl@gmail.com>
2019-10-07 19:30:17 -0500

    Inicio de tripletas

--- Quindim/Clases/ConexionMatriz.cs
+++ Quindim/Clases/ConexionMatriz.cs
@@ -20,7 +20,7 @@ namespace Quindim.Clases
         public static bool ProbarConexion(string serverName)
         {
            // SqlConnection con = new SqlConnection("Data Source=" + System.Environment.MachineName + "\\" + serverName + "; Initial Catalog = LENGUAJE; Server=" + System.Environment.MachineName + "\\" + serverName + " ;Integrated Security = SSPI; Trusted_Connection=True; MultipleActiveResultSets=True");
-            SqlConnection con = new SqlConnection("Data Source=" + System.Environment.MachineName+"; Initial Catalog = PROTOLENGUAJE; Server=" + System.Environment.MachineName + "\\" + Program.serverInstace + " ;Integrated Security = SSPI; Trusted_Connection=True; MultipleActiveResultSets=True");
+            SqlConnection con = new SqlConnection("Data Source=" + System.Environment.MachineName+"; Initial Catalog = PROTOLENGUAJE; Server=" + System.Environment.MachineName + "\\" + serverName + " ;Integrated Security = SSPI; Trusted_Connection=True; MultipleActiveResultSets=True");
             try
             {
                 con.Open();
